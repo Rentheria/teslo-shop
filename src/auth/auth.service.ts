69872069eb1f840f6ed049a1c0536bce75bbbc3b
Manuel Alejandro Hernandez Renteria <manuel.hr@globant.com>
2024-10-08 12:24:49 -0600

    Added Passport, JWT, JWTStragtegy and more JWT

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,11 +3,15 @@ import {
   Injectable,
   InternalServerErrorException,
   Logger,
+  UnauthorizedException,
 } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { CreateUserDto, LoginUserDto } from './dto';
+
 @Injectable()
 export class AuthService {
   private readonly logger = new Logger('AuthService');
@@ -19,8 +23,37 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...userData } = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 319028),
+      });
+
       await this.userRepository.save(user);
+      delete user.password;
+
+      return user;
+    } catch (error) {
+      this.handleDBErrors(error);
+    }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    try {
+      const { password, email } = loginUserDto;
+      const user = await this.userRepository.findOne({
+        where: { email },
+        select: { email: true, password: true },
+      });
+
+      if (!user) {
+        throw new UnauthorizedException(`Credentials are not valid (email)`);
+      }
+
+      if (!bcrypt.compareSync(password, loginUserDto.password)) {
+        throw new UnauthorizedException(`Credentials are not valid (password)`);
+      }
+
       return user;
     } catch (error) {
       this.handleDBErrors(error);
@@ -30,6 +63,8 @@ export class AuthService {
   private handleDBErrors(error: any): never {
     if (error.code === '23505') {
       throw new BadRequestException(error.detail);
+    }
+    if (error) {
     }
     this.logger.error(error);
     throw new InternalServerErrorException(
